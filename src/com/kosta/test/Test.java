8c656bb5e6be356e550210089d30e600ebabffaa
KOSTA <KOSTA@192.168.0.113>
2019-11-04 18:48:51 +0900

    cc업데이트

--- src/com/kosta/test/Test.java
+++ src/com/kosta/test/Test.java
@@ -5,15 +5,16 @@ public class Test {
 	public void aa() {
 		System.out.println("테스트 테스트");
 		System.out.println();
-
 	}
+	
 	
 	public void bb() {
 		System.out.println("왜 안올라가지?");
 	}
 	
 	public void cc() {
-		System.out.println("zz");
+		System.out.println("asdfdfasdfasdfasdf");
+		System.out.println("ffffffff");
 	}
 	
 	public void dd() {
